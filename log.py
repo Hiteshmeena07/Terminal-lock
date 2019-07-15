d31b144bc4701e4dad4c5e998161243e723d1d2d
HITESH KUMAR MEENA <HMEENA040@GMAIL.COM>
2019-07-15 16:29:55 -0700

    Add files via upload

---
+++ log.py
@@ -0,0 +1,23 @@
+import os
+from getpass import getpass
+os.system("clear")
+print(" ")
+os.system("date")
+print("  ")
+os.system("toilet --metal HackeR")
+print("  ")
+
+while True:
+
+        try:
+                user=input("UserName : ")
+                if user == ("Hitesh"):
+                        password=getpass("Password : ")
+                        if password==("HItesh@07"):
+                                break
+        except:
+                print("Fuck Off AssHole,")
+
+print(" ")
+print("WelComE SiR......:) " )
+print(" ")
